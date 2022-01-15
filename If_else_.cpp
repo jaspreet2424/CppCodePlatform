59e484616e9ed831783cde408f75cce8186fd3c1
Jaspreet Singh <js9316713287@gmail.com>
2022-01-15 07:46:42 -0800

    Modified output

--- If_else_.cpp
+++ If_else_.cpp
@@ -9,11 +9,11 @@ using namespace std;
 
     if (num%2 == 0)
     {
-        cout<<"The given number is even "<<num;
+        cout<<"The given number is even : "<<num;
     }
     else
     {
-        cout<<"The given number is not even "<<num;
+        cout<<"The given number is not even : "<<num;
         
     }
     return 0;
